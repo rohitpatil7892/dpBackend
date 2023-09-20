d9cdbb8b5ce7be7b222fd1bff99dc4b7a1fe1b33
Rohit Patil <patilrohit298@gmail.com>
2023-09-20 21:01:57 +0530

    init

--- server.js
+++ server.js
@@ -1,43 +1,50 @@
-const express = require("express");
-const cors = require("cors");
-
+const express = require('express');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+require('./passport');
 const app = express();
+const dotenv = require('dotenv');
+  
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
-app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
-const db = require("./app/models");
-db.sequelize.sync()
-  .then(() => {
-    console.log("Synced db.");
-  })
-  .catch((err) => {
-    console.log("Failed to sync db: " + err.message);
-  });
-
-// // drop the table if it already exists
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+dotenv.config();
+  
+app.use(cookieSession({
+    name: 'google-auth-session',
+    keys: ['key1', 'key2']
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+    
+  
+app.get('/', (req, res) => {
+    res.send("<button><a href='/auth'>Login With Google</a></button>")
 });
-
-require("./app/routes/turorial.routes")(app);
-
-// set port, listen for requests
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  
+// Auth 
+app.get('/auth' , passport.authenticate('google', { scope:
+    [ 'email', 'profile' ]
+}));
+  
+// Auth Callback
+app.get( '/auth/callback',
+    passport.authenticate( 'google', {
+        successRedirect: '/auth/callback/success',
+        failureRedirect: '/auth/callback/failure'
+}));
+  
+// Success 
+app.get('/auth/callback/success' , (req , res) => {
+    if(!req.user)
+        res.redirect('/auth/callback/failure');
+    res.send("Welcome " + req.user.email);
+});
+  
+// failure
+app.get('/auth/callback/failure' , (req , res) => {
+    res.send("Error");
+})
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
